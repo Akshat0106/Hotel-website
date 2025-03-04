75805c9fab5f46b8760d2b1905f1936197e66de9
Akshat Gupta <akshatgupta0401@gmail.com>
2025-03-04 13:45:19 +0530

    add the map html tag

--- src/pages/GetInTouch.jsx
+++ src/pages/GetInTouch.jsx
@@ -1,11 +1,10 @@
 export function GetInTouch() {
   return (
     <>
-      <div className=" flex justify-center items-center h-screen w-full mt-20">
-        <div className="">
+      <div className=" flex flex-wrap h-screen w-full border-2 border-black mt-32">
+        <div className="w-full">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3769.8655011804153!2d72.89023277497827!3d19.113555482098764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c92d5e58cdc9%3A0x94c2dec7f68a1d1b!2sBoomerang%20Building!5e0!3m2!1sen!2sin!4v1740981511034!5m2!1sen!2sin"></iframe>
         </div>
-        Hello
       </div>
     </>
   );
