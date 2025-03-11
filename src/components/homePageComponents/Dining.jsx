e20e91734cd5a08746aa33303053fc42837286c0
Akshat Gupta <akshatgupta0401@gmail.com>
2025-03-11 14:04:55 +0530

    added the border to the hexagon shape

--- src/components/homePageComponents/Dining.jsx
+++ src/components/homePageComponents/Dining.jsx
@@ -1,35 +1,117 @@
+// // export function Dining({ img1 }) {
+// //   return (
+// //     <div>
+// //       <div className="p-4 mt-2 ">
+// //         <div className="text-center text-4xl text-white font-bold  ">
+// //           Dining
+// //         </div>
+
+// //         <div className="flex flex-row items-center justify-center border-2 mt-4 h-full gap-56">
+// //           <div className="relative w-60 h-60">
+// //             <img
+// //               src={img1}
+// //               alt="Hexagon Image"
+// //               className="w-full h-full object-cover clip-hexagon"
+// //             />
+// //           </div>
+
+// //           <div className="relative w-60 h-60">
+// //             <img
+// //               src={img1}
+// //               alt="Hexagon Image"
+// //               className="w-full h-full object-cover clip-hexagon"
+// //             />
+// //           </div>
+
+// //           <div className="relative w-60 h-60">
+// //             <img
+// //               src={img1}
+// //               alt="Hexagon Image"
+// //               className="w-full h-full object-cover clip-hexagon"
+// //             />
+// //           </div>
+// //         </div>
+// //       </div>
+// //     </div>
+// //   );
+// // }
+
 // export function Dining({ img1 }) {
 //   return (
-//     <div>
-//       <div className="p-4 mt-2 ">
-//         <div className="text-center text-4xl text-white font-bold  ">
-//           Dining
+//     <div className="p-4 mt-2">
+//       {/* Section Heading */}
+//       <div className="text-center text-3xl md:text-4xl text-white font-bold">
+//         Dining
+//       </div>
+
+//       {/* Image Grid */}
+//       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-6 place-items-center">
+//         <div className="relative w-48 h-48 md:w-60 md:h-60 border-gray-500">
+//           <img
+//             src={img1}
+//             alt="Hexagon Image"
+//             className="w-full h-full object-cover clip-hexagon"
+//           />
 //         </div>
 
-//         <div className="flex flex-row items-center justify-center border-2 mt-4 h-full gap-56">
-//           <div className="relative w-60 h-60">
-//             <img
-//               src={img1}
-//               alt="Hexagon Image"
-//               className="w-full h-full object-cover clip-hexagon"
-//             />
-//           </div>
+//         <div className="relative w-48 h-48 md:w-60 md:h-60 border-gray-500">
+//           <img
+//             src={img1}
+//             alt="Hexagon Image"
+//             className="w-full h-full object-cover clip-hexagon"
+//           />
+//         </div>
 
-//           <div className="relative w-60 h-60">
-//             <img
-//               src={img1}
-//               alt="Hexagon Image"
-//               className="w-full h-full object-cover clip-hexagon"
-//             />
-//           </div>
+//         <div className="relative w-48 h-48 md:w-60 md:h-60">
+//           <img
+//             src={img1}
+//             alt="Hexagon Image"
+//             className="w-full h-full object-cover clip-hexagon border-4 border-gray-500"
+//           />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
-//           <div className="relative w-60 h-60">
-//             <img
-//               src={img1}
-//               alt="Hexagon Image"
-//               className="w-full h-full object-cover clip-hexagon"
-//             />
-//           </div>
+// export function Dining({ img1 }) {
+//   return (
+//     <div className="p-4 mt-2">
+//       {/* Section Heading */}
+//       <div className="text-center text-3xl md:text-4xl text-white font-bold">
+//         Dining
+//       </div>
+
+//       {/* Image Grid */}
+//       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-6 place-items-center">
+//         {/* Hexagon with border */}
+//         <div className="relative w-48 h-48 md:w-60 md:h-60 hexagon-container">
+//           <div className="hexagon-border"></div>
+//           <img
+//             src={img1}
+//             alt="Hexagon Image"
+//             className="hexagon-image"
+//           />
+//         </div>
+
+//         {/* Second Hexagon */}
+//         <div className="relative w-48 h-48 md:w-60 md:h-60 hexagon-container">
+//           <div className="hexagon-border"></div>
+//           <img
+//             src={img1}
+//             alt="Hexagon Image"
+//             className="hexagon-image"
+//           />
+//         </div>
+
+//         {/* Third Hexagon */}
+//         <div className="relative w-48 h-48 md:w-60 md:h-60 hexagon-container">
+//           <div className="hexagon-border"></div>
+//           <img
+//             src={img1}
+//             alt="Hexagon Image"
+//             className="hexagon-image"
+//           />
 //         </div>
 //       </div>
 //     </div>
@@ -46,32 +128,31 @@ export function Dining({ img1 }) {
 
       {/* Image Grid */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-6 place-items-center">
-        <div className="relative w-48 h-48 md:w-60 md:h-60 border-gray-500">
-          <img
-            src={img1}
-            alt="Hexagon Image"
-            className="w-full h-full object-cover clip-hexagon"
-          />
+        {/* First Hexagon */}
+        <div className="hex-wrapper w-48 h-48 md:w-60 md:h-60">
+          <div className="hex-border"></div>
+          <div className="hex-content">
+            <img src={img1} alt="Hexagon Image" className="w-full h-full object-cover" />
+          </div>
         </div>
 
-        <div className="relative w-48 h-48 md:w-60 md:h-60 border-gray-500">
-          <img
-            src={img1}
-            alt="Hexagon Image"
-            className="w-full h-full object-cover clip-hexagon"
-          />
+        {/* Second Hexagon */}
+        <div className="hex-wrapper w-48 h-48 md:w-60 md:h-60">
+          <div className="hex-border"></div>
+          <div className="hex-content">
+            <img src={img1} alt="Hexagon Image" className="w-full h-full object-cover" />
+          </div>
         </div>
 
-        <div className="relative w-48 h-48 md:w-60 md:h-60">
-          <img
-            src={img1}
-            alt="Hexagon Image"
-            className="w-full h-full object-cover clip-hexagon border-4 border-gray-500"
-          />
+        {/* Third Hexagon */}
+        <div className="hex-wrapper w-48 h-48 md:w-60 md:h-60">
+          <div className="hex-border"></div>
+          <div className="hex-content">
+            <img src={img1} alt="Hexagon Image" className="w-full h-full object-cover" />
+          </div>
         </div>
       </div>
     </div>
   );
 }
-
 
